0469b97aeb1b7ed339dc898a4d244a464898280b
dan4inatora <tolev98@abv.bg>
2018-12-26 07:51:34 +1300

    Update Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -1,155 +1,175 @@
 #include <iostream>
 
-template<typename T>
-class Queue{
-public:
-    struct Node{
-        T data;
-        Node * next;
-        Node(T data, Node * next):
-            data(data),
-            next(next){
-        }
-    };
+using namespace std;
 
-    Queue();
-    Queue(const Queue& other);
-    Queue& operator = (const Queue& other);
-    ~Queue();
-    T& front()const;
-    T& back()const;
-    void push(const T& elem);
-    void pop();
-    size_t getSize()const;
-    void clear();
-    void print()const;
+bool fexists(const std::string& filename) {
+  ifstream ifile(filename.c_str());
+  return (bool)ifile;
+}
+
+template <typename T>
+struct Node
+{
+    T data;
+    Node<T>* next;
+
+    Node(const T& dt,Node<T>* nxt = NULL)
+    {
+        data = dt;
+        next = nxt;
+    }
+
+};
+template <typename T>
+class queue
+{
 
 private:
-    Node * first;
-    Node * last;
-    size_t size;
-    void copy(const Queue& other);
-    void erase();
-    bool isEmpty()const;
+    Node<T>* first;
+    Node<T>* last;
+    size_t counter;
+
+public:
+    queue();
+    queue(const queue&);
+    queue& operator=(const queue&);
+    void push(const T&);
+    void pop();
+    T front() const;
+    T end() const;
+    size_t size() const;
+    void print()const;
+    bool isEmpty() const;
+
+
+private:
+    void copy(const queue&);
+    void init();
+    void empty();
 
 };
 
-template<typename T>
-void Queue<T> :: push(const T& elem){
-    Node * temp = new Node(elem, nullptr);
-    if(isEmpty()){
-        this->first = temp;
-        this->last = temp;
-    }
-    else{
-        this->last->next = temp;
-        this->last = temp;
-    }
-    this->size++;
-}
-
-template<typename T>
-bool Queue<T> :: isEmpty()const{
-    return this->size == 0;
-}
-
-template<typename T>
-size_t Queue<T> :: getSize()const{
-    return this->size;
-}
-
-template<typename T>
-Queue<T> :: Queue(){
-    this->first = nullptr;
-    this->last = nullptr;
-    this->size=0;
-}
-
-template<typename T>
-void Queue<T> :: copy(const Queue& other){
-
-    Node * temp = other.first;
-    for(size_t i = 0; i < other.getSize(); i++){
-        this->push(temp->data);
-        temp=temp->next;
-    }
-}
-
-template<typename T>
-Queue<T> :: Queue(const Queue& other){
-    this->copy(other);
-}
-
-template<typename T>
-Queue<T>& Queue<T> :: operator = (const Queue& other){
-    if(this != &other){
-        this->erase();
-        this->copy(other);
-    }
-    return *this;
-}
-
-template<typename T>
-Queue<T> ::~Queue(){
-    this->erase();
-}
-
-template<typename T>
-void Queue<T> :: pop(){
-    if(!isEmpty()){
-        Node*temp = this->first;
-        this->first = this->first->next;
-        delete[]temp;
-        size--;
-    }
-}
-
-template<typename T>
-void Queue<T> :: erase(){
-    while(!isEmpty()){
-        this->pop();
-    }
-}
-
-template<typename T>
-T& Queue<T> :: front()const{
-    if(this->first != nullptr){
-        return this->first->data;
-    }
-}
-
-template<typename T>
-T& Queue<T> :: back()const{
-    if(this->last != nullptr){
-        return this->last->data;
-    }
-}
-
-template<typename T>
-void Queue<T> :: clear(){
-    this->erase();
-    this->first = nullptr;
-    this->last = nullptr;
-    this->size=0;
-}
-
-template<typename T>
-void Queue<T> :: print()const{
-    Node * temp = this->first;
-    for(size_t i = 0; i < this->size; i++){
+template <typename T>
+void queue<T>:: print()const{
+        Node<T> * temp = this->first;
+    for(size_t i = 0; i < this->counter; i++){
         std::cout<<temp->data<<std::endl;
         temp = temp->next;
     }
 }
-int main()
+
+template <typename T>
+queue<T>::queue()
 {
-    Queue<int> q;
-    q.push(3);
-    q.push(2);
-    q.push(1);
-    q.push(4);
-    q.print();
-    std::cout<<q.back()<<std::endl;
-    std::cout<<q.front()<<std::endl;
-    return 0;
+    this->init();
+}
+template <typename T>
+queue<T>::queue(const queue& q)
+{
+    this->init();
+    this->copy(q);
+}
+
+template <typename T>
+queue<T>& queue<T>::operator=(const queue& q)
+{
+    if(this != &q)
+    {
+        this->empty();
+        this->init();
+        this->copy(q);
+    }
+
+    return *this;
+}
+template <typename T>
+void queue<T>::push(const T& data)
+{
+    Node<T>* add = new Node<T>(data,NULL);
+    if(add)
+    {
+        if(this->isEmpty())
+        {
+            this->first = add;
+        }
+        else
+        {
+            this->last->next = add;
+        }
+
+        this->last = add;
+        this->counter++;
+    }
+}
+template <typename T>
+void queue<T>::pop()
+{
+    if(!this->isEmpty())
+    {
+        Node<T>* remove = this->first;
+        if(remove)
+        {
+            if(this->size() == 1)
+            {
+                this->init();
+            }
+            else
+            {
+            this->first = this->first->next;
+            delete remove;
+            this->counter--;
+            }
+        }
+    }else cout<<"Empty queue"<<endl;
+}
+template <typename T>
+T queue<T>::front() const
+{
+    if(!this->isEmpty())
+    return this->first->data;
+
+    else return 0;
+}
+template <typename T>
+T queue<T>::end() const
+{
+    if(!this->isEmpty())
+    return this->last->data;
+
+    else return 0;
+}
+template <typename T>
+size_t queue<T>::size() const
+{
+    return this->counter;
+}
+
+template <typename T>
+bool queue<T>::isEmpty() const
+{
+    return this->counter == 0;
+}
+template <typename T>
+void queue<T>::copy(const queue& q)
+{
+
+    Node<T>* node = q.first;
+    for(size_t i = 0; i < q.size(); i++)
+    {
+        this->push(node->data);
+        node = node->next;
+    }
+}
+template <typename T>
+void queue<T>::init()
+{
+    this->first = NULL;
+    this->last = NULL;
+    this->counter = 0;
+}
+template <typename T>
+void queue<T>::empty()
+{
+    for(size_t i = 0; i < this->size(); i++)
+        this->pop();
 }
